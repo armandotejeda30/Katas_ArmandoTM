b8c919aa11250bdb18caaa78ab8dc0334882a451
armandotejeda30 <99111235+armandotejeda30@users.noreply.github.com>
2022-02-17 23:26:03 -0700

    Add files via upload

---
+++ kata01.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+# Primer programa Fecha
+print("Fecha")
+print("El dia de hoy es: " + str(date.today()))
+
+
+#Segundo programa convertidor de unidades
+parsec = 11
+lightyears = 3.26156 * parsec
+print("Convertidor de unidades")
+print(str(parsec) + " parsec, son " + str(lightyears) + " lightyears")
+
+
